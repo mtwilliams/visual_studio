6b923f7bce0e3b3c4e191d5af4314009e85bbd36
Michael Williams <m.t.williams@live.com>
2016-03-06 20:42:23 -0800

    Bump version.

--- lib/visual_studio/gem.rb
+++ lib/visual_studio/gem.rb
@@ -41,7 +41,7 @@ module VisualStudio
     end
 
     module VERSION #:nodoc:
-      MAJOR, MINOR, PATCH, PRE = [0, 1, 0, 1]
+      MAJOR, MINOR, PATCH, PRE = [0, 1, 0, 2]
       STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
     end
 
