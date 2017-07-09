0cd095ec375717983edb1ed6c77886dd395e0e72
Michael Williams <m.t.williams@live.com>
2017-07-08 17:15:24 -0700

    Adjust `VisualStudio::Environment.merge` to be case insensitive.

--- lib/visual_studio/environment.rb
+++ lib/visual_studio/environment.rb
@@ -3,10 +3,14 @@ module VisualStudio
     SEARCH_DIRECTORIES = ['PATH', 'INCLUDE', 'LIB', 'LIBPATH']
 
     def self.merge(base, overlay)
+      # We merge case insensitively, because Windows. We treat |base| case as canonical
+      # unless the variable is only named by |overlay|.
+      cased = (overlay.keys + base.keys).uniq.map{|key| [key.upcase, key]}.to_h
+
       # TODO(mtwilliams): Verify match-up between VCInstallDir and VisualStudioVersion?
       # TODO(mtwilliams): Rederive environment variables based on VCInstallDir and/or WindowsSdkDir.
-      env = base.to_h.merge(overlay.to_h) do |variable, base, overlay|
-        if SEARCH_DIRECTORIES.include? variable
+      env = canonicalize(base).merge(canonicalize(overlay)) do |variable, base, overlay|
+        if SEARCH_DIRECTORIES.include? variable.upcase
           # TODO(mtwilliams): Detect new Visual Studio or Windows SDK related
           # paths and replace the old ones based on that.
           base    = base.split(';')
@@ -29,6 +33,15 @@ module VisualStudio
           overlay
         end
       end
+
+      env.map { |canonical, value|
+        [cased[canonical], value]
+      }.to_h
     end
+
+    private
+      def self.canonicalize(environment)
+        environment.to_h.map{|key, value| [key.upcase, value]}.to_h
+      end
   end
 end
